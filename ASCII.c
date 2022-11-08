a204a17619cdeaa7f83a7dc3a871c5809c46fa02
ATEN <psukhyxd@gmail.com>
2022-11-08 23:12:18 +0700

    TestC

---
+++ ASCII.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main ()
+{
+char chl, ch2, ch3;
+ch1='A';
+ch2=66;
+ch3=ch1+32;
+printf ("ASCII value of %c = %d\n", chl, chl);
+printf("ASCII value of %c = %d\n", ch2, ch2);
+printf ("ASCII value of %c = %d\n", ch3, ch3);
+getch();
+return 0;
+} 
